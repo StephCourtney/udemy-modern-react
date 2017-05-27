3e93f000182dc63cae6493f0497f6758ea73c723
Steph Courtney <scourtn4@ie.ibm.com>
2017-05-27 10:55:25 +0100

    setup component

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Step 1: Create a new component that produces some HTML
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+// Step 2: Take this components generated HTML and
+//        put it in the DOM
+
+ReactDOM.render(<App />, document.querySelector('.container'));
