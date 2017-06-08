40609a963619530fc37f487c6bdc9571c6f16968
Steph Courtney <scourtn4@ie.ibm.com>
2017-06-08 09:08:27 +0100

    added sparklines

--- WeatherApp/src/containers/weather_list.js
+++ WeatherApp/src/containers/weather_list.js
@@ -1,13 +1,24 @@
 import React, { Component} from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
 
     renderWeather(cityData) {
         const name = cityData.city.name;
+        //ES6: weather => weather.main.temp same as
+        // map(function(weather) {
+        //   return weather.main.temp; 
+        // })
+        // return keyword is implied with fat arrow as long as it is on the same line
+        const temps = cityData.list.map(weather => weather.main.temp);
+        console.log(temps);
         return (
             <tr key={name}>
                 <td>{name}</td>
+                <td>
+                    <Chart data={temps} color="orange" />
+                </td>
             </tr>
         );
     }
